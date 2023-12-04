0e98914bba928eda1d1b90a5366793175209e877
shotout <hevaldo66@gmail.com>
2023-12-04 10:24:53 +0700

    adjust story response

--- app/Http/Controllers/Api/v1/StoryController.php
+++ app/Http/Controllers/Api/v1/StoryController.php
@@ -109,7 +109,7 @@ class StoryController extends Controller
         }
 
         $en = str_split($data[0]->content_en,1000);
-        $data->put('content_en', $en);
+        $data->put(1,$en);
         // retun response
         return response()->json([
             'status' => 'success',
