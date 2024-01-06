f20e197b5dd24a0ac3c4e3a3527f04434aabc4e9
shotout <hevaldo66@gmail.com>
2024-01-06 09:53:41 +0700

    adjust purchasely respond

--- app/Http/Controllers/Api/v1/PurchaselyController.php
+++ app/Http/Controllers/Api/v1/PurchaselyController.php
@@ -14,42 +14,44 @@ class PurchaselyController extends Controller
         $data = $request->all();
         $user = User::with('subscription')->where('purchasely_id', $data['anonymous_user_id'])->first();
 
-        $user->subscription->purchasely_data = $data;
-        $user->subscription->update();
-
-        // if ($user && $data['next_renewal_at'] && $data['next_renewal_at'] != '') {
-        //     if ($data['event_name'] == 'ACTIVATE') {
-        //         if ($data['plan'] === 'ErotalesUnlimitedStoriesandAudioAnnual') {
-        //             $type = 3;
-        //         } else if ($data['plan'] === 'Erotales_unlimitted_stories_annual') {
-        //             $type = 2;
-        //         }
+        if ($user && $data['next_renewal_at'] ) {
+            if ($data['event_name'] == 'ACTIVATE') {
+                if ($data['plan'] === 'ErotalesUnlimitedStoriesandAudioAnnual') {
+                    $type = 3;
+                } else if ($data['plan'] === 'Erotales_unlimitted_stories_annual') {
+                    $type = 2;
+                }
 
 
-        //         $user->is_member = 1;
-        //         $user->update();
+                $user->is_member = 1;
+                $user->update();
 
-        //         $user->subscription->plan_id = $type;
-        //         $user->subscription->type = $type;
-        //         $user->subscription->started = date('Y-m-d', strtotime($data['purchased_at']));
-        //         $user->subscription->renewal = date('Y-m-d', strtotime($data['next_renewal_at']));
-        //         $user->subscription->purchasely_data = $data;
-        //         $user->subscription->update();
-        //     }
+                $user->subscription->plan_id = $type;
+                $user->subscription->type = $type;
+                $user->subscription->started = date('Y-m-d', strtotime($data['purchased_at']));
+                $user->subscription->renewal = date('Y-m-d', strtotime($data['next_renewal_at']));
+                $user->subscription->purchasely_data = $data;
+                $user->subscription->update();
+            }
 
-        //     if ($data['event_name'] == 'DEACTIVATE') {
-        //         $user->is_member = 0;
-        //         $user->update();
+            if ($data['event_name'] == 'DEACTIVATE') {
+                $user->is_member = 0;
+                $user->update();
 
-        //         $user->subscription->plan_id = 1;
-        //         $user->subscription->type = 1;
-        //         $user->subscription->started = null;
-        //         $user->subscription->renewal = null;
-        //         $user->subscription->subscription_data = null;
-        //         $user->subscription->purchasely_data = $data;
-        //         $user->subscription->update();
-        //     }
-        // }
+                $user->subscription->plan_id = 1;
+                $user->subscription->type = 1;
+                $user->subscription->started = null;
+                $user->subscription->renewal = null;
+                $user->subscription->subscription_data = null;
+                $user->subscription->purchasely_data = $data;
+                $user->subscription->update();
+            }
+        }
+        else
+        {
+            $user->subscription->stripe_status = $data;
+            $user->subscription->update();
+        }
 
         Log::info($data);
 
