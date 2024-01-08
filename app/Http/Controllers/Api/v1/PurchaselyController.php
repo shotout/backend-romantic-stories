2663a6d9f6f14134a73e5333369fe25279ae35a9
shotout <hevaldo66@gmail.com>
2024-01-08 13:02:56 +0700

    adjust purchasely respond

--- app/Http/Controllers/Api/v1/PurchaselyController.php
+++ app/Http/Controllers/Api/v1/PurchaselyController.php
@@ -49,7 +49,7 @@ class PurchaselyController extends Controller
             }
         }
         if($user){
-            $user->subscription_data = $data;
+            $user->subscription->subscription_data = $data;
             $user->update();
         }
 
